f2a0279741f599bc62a987b4f013109689bb1b40
wangsong <18280296472@163.c0m>
2020-06-02 22:14:07 +0800

    add graph framework

---
+++ graph/graph/graph.h
@@ -0,0 +1,25 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+
+template<typename T>
+class Graph
+{
+    enum vertexColor
+    {
+        White,
+  
+    }
+public:
+
+
+private:
+
+};
+
+
+#endif
