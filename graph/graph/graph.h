79e9365fe8d11576c8242ef28609313ba21159ae
wangsong <18280296472@163.c0m>
2020-06-07 09:18:10 +0800

    add graph interface

--- graph/graph/graph.h
+++ graph/graph/graph.h
@@ -9,16 +9,96 @@
 template<typename T>
 class Graph
 {
-    enum vertexColor
+public:
+    enum VertexColor
     {
         White,
-  
-    }
+        Gray,
+        Black,
+    };
+    enum EdgeType
+    {
+        Tree,
+        Backward,
+        Forward,
+        Cross,
+    };
+    //对于每个顶点u，将权值为w的边(u, v)的信息(v, w)存放于此
+    using AdjInfo = std::pair<int, T>;
+    //顶点的邻接表
+    using AdjInfoList = std::vector<AdjInfo>;
+    using VertexAdjList = std::pair<int, AdjInfoList>;
+
 public:
+    Graph(){}
+    virtual ~Graph(){}
+    inline const int& vertexNumber() const {return mAdjList.size();}
+    bool isEdge(int u, int v)
+    {
+        for(auto & adjList : mAdjList) {
+            if(adjList.first == u) {
+                for(auto& adjInfo : adjList.second) {
+                    if (adjInfo.first == v)
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
 
+    bool isEdge(int u, int v, T& weight) 
+    {
+        for(auto & adjList : mAdjList) {
+            if(adjList.first == u) {
+                for(auto& adjInfo : adjList.second) {
+                    if (adjInfo.first == v) {
+                        weight = adjInfo.second;
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 
+    void addEdge(int u, int v, T weight = 1) {
+        auto uIter = std::find_if(mAdjList.begin(), mAdjList.end(), [&](VertexAdjList Iu){return Iu.first == u;});
+        if(uIter != mAdjList.end()){
+           auto vIter = std::find_if(uIter->second.begin(), uIter->second.end(), [&](AdjInfo Iv){return Iv.first == v;});
+           if(vIter != uIter->second.end()) {
+               vIter->second = weight;
+           } else {
+               uIter->second.emplace_back(v, weight);
+           }
+        } else {
+            AdjInfoList ail{{v, weight}};
+            mAdjList.emplace_back(u, ail);
+        }
+    }
+
+    void removeEdge(int u, int v) {
+        auto uIter = std::find_if(mAdjList.begin(), mAdjList.end(), [&](VertexAdjList Iu){return Iu.first == u;});
+        if(uIter != mAdjList.end()){
+            auto vIter = std::find_if(uIter->second.begin(), uIter->second.end(), [&](AdjInfo Iv){return Iv.first == v;});
+            if(vIter != uIter->second.end()) {
+                uIter->second.erase(vIter);
+            }
+            if(uIter->second.empty()) {
+                mAdjList.erase(uIter);
+            }
+        }
+    }
+
+    AdjInfoList findAdjInfoList(int u){
+        auto uIter = std::find_if(mAdjList.begin(), mAdjList.end(), [&](VertexAdjList Iu){return Iu.first == u;});
+        if(uIter != mAdjList.end()){
+            return uIter->second;
+        } else {
+            return AdjInfoList();
+        }
+    }
 private:
-
+    std::vector<VertexAdjList> mAdjList;
 };
 
 
--- graph/graph/main.cpp
+++ graph/graph/main.cpp
@@ -3,6 +3,12 @@
 
 int main(int argc, char**argv)
 {
+    Graph<int> g;
+    g.addEdge(0, 1, 2);
+    g.addEdge(0, 1, 3);
+    g.addEdge(0, 2, 1);
+    g.removeEdge(0, 2);
+    Graph<int>::AdjInfoList adjList = g.findAdjInfoList(0);
 
     return 0;
 }
