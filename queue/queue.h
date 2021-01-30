bd80e9b80e6adf12719e5f192d12b81fb1b10d82
ws <18280296472@163.c0m>
2021-01-30 22:02:46 +0800

    add queue

--- queue/queue.h
+++ queue/queue.h
@@ -1,54 +1,65 @@
 /**
  * Copyright (c) 2020-2021
  *
- * @file stack.h
- * @brief stack class
+ * @file queue.h
+ * @brief queue using list data struction
  * @version 1.0.0
  * @author wang song (taokong1017@163.com)
  * @date 2021-01-30
  */
-#ifndef STACK_H
-#define STACK_H
+#ifndef QUEUE_H
+#define QUEUE_H
 
 #include <cstddef>
 #include <iostream>
 
-template <typename Item>
-class Stack
+template<typename Item>
+class Queue
 {
     typedef struct Node {
         Item item;
         Node* next;
     } Node_t;
 public:
-    Stack( )
+    Queue( )
     : mHead(nullptr)
+    , mTail(nullptr)
     , mCount(0)
     {
 
     }
 
-    virtual ~Stack( ) {
-        while (!empty( )) {
-            pop( );
+    virtual ~Queue( ) {
+        while (!empty( )){
+            dequeue( );
         }
     }
 
-    void push(Item item) {
-        mCount++;
+    void enqueue(Item item) {
         Node_t* node = new Node_t;
         node->item = item;
-        node->next = mHead;
-        mHead = node;
+
+        if (!mHead) {
+            mHead = node;
+        }
+        if (mTail) {
+            mTail->next = node;
+        }
+        mTail = node;
+        mCount++;
     }
 
-    Item pop( ) {
+    Item dequeue( ) {
+        Node_t* node = mHead;
+        mHead = node->next;
+        Item item = node->item;
+        delete node;
         mCount--;
-        Node_t* first = mHead;
-        mHead = mHead->next;
-        Item item = first->item;
-        delete first;
         return item;
+    }
+
+    size_t size( ) {
+        return mCount;
     }
 
     bool empty( ) {
@@ -65,6 +76,7 @@ public:
 
 private:
     Node_t* mHead;
+    Node_t* mTail;
     size_t mCount;
 };
 
